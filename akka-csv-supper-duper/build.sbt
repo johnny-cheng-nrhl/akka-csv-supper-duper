0ce2d6bfd6fe0edbb6345f22a045bbf9d0115729
Johnny Cheng <johnny.cheng@hautelook.com>
2019-08-07 16:31:05 -0700

    Third Commit
    - Add string to format the update sql
    - Add joda datetime dependency

--- akka-csv-supper-duper/build.sbt
+++ akka-csv-supper-duper/build.sbt
@@ -1,19 +1,30 @@
-name := "akka-quickstart-scala"
-
-version := "1.0"
-
-scalaVersion := "2.12.6"
-
 lazy val akkaVersion = "2.5.22"
 lazy val alpakkaVersion = "1.0.2"
+lazy val nscalaVersion = "2.20.0"
 
-libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
-  "com.lightbend.akka" %% "akka-stream-alpakka-elasticsearch" % alpakkaVersion,
-  "com.lightbend.akka" %% "akka-stream-alpakka-csv" % alpakkaVersion,
-  "com.lightbend.akka" %% "akka-stream-alpakka-file" % alpakkaVersion,
-  "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+lazy val root = (project in file(".")).settings(
+  inThisBuild(
+    List(
+      organization := "com.hautelook.catalog",
+      scalaVersion := "2.12.6",
+      version := "0.1.3-SNAPSHOT"
+    )
+  ),
+  name := "akka-quickstart-scala",
+  libraryDependencies ++= Seq(
+    "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+    "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+    "com.lightbend.akka" %% "akka-stream-alpakka-elasticsearch" % alpakkaVersion,
+    "com.lightbend.akka" %% "akka-stream-alpakka-csv" % alpakkaVersion,
+    "com.lightbend.akka" %% "akka-stream-alpakka-file" % alpakkaVersion,
+    // Nscala Time
+    "com.github.nscala-time" %% "nscala-time" % nscalaVersion,
+    "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+  ),
+  scalacOptions ++= Seq(
+    "-language:higherKinds", // Allow higher-kinded types
+    "-Ypartial-unification"
+  )
 )
 
 // set the main class for the main 'sbt run' task
--- akka-csv-supper-duper/src/main/scala/net/propoint/fun/CsvConsumer.scala
+++ akka-csv-supper-duper/src/main/scala/net/propoint/fun/CsvConsumer.scala
@@ -8,7 +8,8 @@ import akka.stream.{ActorMaterializer, ClosedShape}
 import akka.stream.alpakka.csv.scaladsl.{CsvParsing, CsvToMap}
 import akka.stream.scaladsl.{FileIO, Flow, GraphDSL, RunnableGraph, Sink}
 import akka.util.ByteString
-import akka.stream.scaladsl._
+import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
@@ -22,18 +23,48 @@ object CsvConsumer extends App {
   // implicit actor materializer
   implicit val materializer = ActorMaterializer()
   
-  val path = Paths.get("src/main/resources/backfill_events1.csv")
+  val path = Paths.get("src/main/resources/backfill_events.csv")
   println(path)
+
+  private def parseOptionalStr(str: String): Option[String] =
+    Option(str).map(_.trim).filter(_.nonEmpty)
+
+  val defaultDateTimeFormatter: DateTimeFormatter = DateTimeFormat.forPattern("MM/dd/yy")
+  val poEventDateTimeFormatter: DateTimeFormatter = DateTimeFormat.forPattern("MM/dd/yy HH:mm")
+  val databaseDateTimeFormatter: DateTimeFormatter = DateTimeFormat.forPattern("MM/dd/yyy HH:mm:ss")
+
+  private def parseOptionalDateTimeStr(str: String): Option[DateTime] =
+    parseOptionalStr(str).map(DateTime.parse(_, poEventDateTimeFormatter))
 
   private val lineByLineSource = FileIO.fromPath(path)
 //    .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 1024))
 //    .map(_.utf8String).drop(1)
+
+  private def makeUpddateString(startDate: DateTime, endDate: DateTime, eventId: Long) =
+    s"UPDATE events SET start_date = str_to_date(CONCAT(date('${startDate.getYear}-${startDate.getMonthOfYear}-${startDate.getDayOfMonth}'), ' ', time('08:00'))," +
+      s" '%Y-%m-%d %H:%i:%s'), end_date = str_to_date(CONCAT(date('${endDate.getYear}-${endDate.getMonthOfYear()}-${endDate.getDayOfMonth}'), ' ', time('08:00')), '%Y-%m-%d %H:%i:%s') " +
+      s"WHERE event_id = $eventId;"
   
   val source = FileIO.fromPath(path)
   val flow1: Flow[ByteString, List[ByteString], NotUsed] = 
     CsvParsing.lineScanner()
   val flow2: Flow[List[ByteString], List[String], NotUsed] = 
     Flow[List[ByteString]].map(list => list.map(_.utf8String))
+  val flow3: Flow[List[String], SalesEvent, NotUsed] =
+    Flow[List[String]].map(
+      l =>
+        SalesEvent(l(0).toLong,
+                  DateTime.parse(l(1), defaultDateTimeFormatter),
+                  DateTime.parse(l(2), defaultDateTimeFormatter),
+                  parseOptionalStr(l(3)).map(_.toLong),
+                  DateTime.parse(l(4), poEventDateTimeFormatter),
+                  DateTime.parse(l(5), poEventDateTimeFormatter))
+    )
+  val flow4: Flow[SalesEvent, SalesEvent, NotUsed] =
+    Flow[SalesEvent].filter(s => s.eventId.isDefined)
+  val flow5: Flow[SalesEvent, String, NotUsed] =
+    Flow[SalesEvent].map(s => makeUpddateString(s.eventStartDate,
+      s.eventEndDate, s.eventId.get))
 
   // val flow3 = Flow[List[String]].map(list => CountryCapital(list(0), list(1)))
 
@@ -41,6 +72,9 @@ object CsvConsumer extends App {
     .via(flow1)
     .via(flow2)
     .drop(1)
+    .via(flow3)
+    .via(flow4)
+    .via(flow5)
     .runForeach(println)
   
   private val reply = Await.result(future, 10 seconds)
@@ -70,3 +104,6 @@ object CsvConsumer extends App {
 //  }
 //  Await.result(system.whenTerminated, Duration.Inf)
 }
+
+case class SalesEvent(poId: Long, startShipDate: DateTime, endShipDate: DateTime,
+                      eventId: Option[Long], eventStartDate: DateTime, eventEndDate: DateTime)
